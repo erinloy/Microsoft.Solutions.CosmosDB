d0fd35b3057434b27bf2b625cc0288c38c9c645c
Erin Loy <eloy@crazydogventures.com>
2023-03-05 07:22:10 -0800

    wip

--- src/Libraries/Microsoft.Solutions.CosmosDB.SQL/SQLEntityCollectionBase.cs
+++ src/Libraries/Microsoft.Solutions.CosmosDB.SQL/SQLEntityCollectionBase.cs
@@ -5,12 +5,10 @@ using Microsoft.Azure.Cosmos;
 using System;
 using System.Diagnostics;
 
-namespace Microsoft.Solutions.CosmosDB.SQL
-{
+namespace Microsoft.Solutions.CosmosDB.SQL {
     public class SQLEntityCollectionBase<TEntity> : IDataRepositoryProvider<TEntity>
-        where TEntity : class, IEntityModel<string>
-    {
-        public IRepository<TEntity, string> EntityCollection { get ;  init ; }
+        where TEntity : class, IEntityModel<string> {
+        public IRepository<TEntity, string> EntityCollection { get; init; }
 
         /// <summary>
         /// 
@@ -18,24 +16,21 @@ namespace Microsoft.Solutions.CosmosDB.SQL
         /// <param name="DataConnectionString">Connection String</param>
         /// <param name="CollectionName">Your Database Name</param>
         /// <param name="ContainerName">(Optional) If you don't pass it, The Container will be created by Entity Model Class Name + "s", In Model First Dev, You don't need to use it</param>
-        public SQLEntityCollectionBase(string DataConnectionString, string CollectionName, string ContainerName = "")
-        {
+        public SQLEntityCollectionBase(string DataConnectionString, string CollectionName, string ContainerName = "", Func<CosmosClient, string, string, string, IRepository<TEntity, string>> repoFactory = null) {
             CosmosClientManager.DataconnectionString = DataConnectionString;
             CosmosClient _client = CosmosClientManager.Instance;
 
-            this.EntityCollection = 
-                new BusinessTransactionRepository<TEntity, string>(_client,
-                CollectionName, ContainerName);
+            this.EntityCollection = repoFactory?.Invoke(_client, DataConnectionString, CollectionName, ContainerName);
+
+            this.EntityCollection ??= new BusinessTransactionRepository<TEntity, string>(_client, CollectionName, ContainerName);
         }
     }
 
-    public sealed class CosmosClientManager
-    {
+    public sealed class CosmosClientManager {
         private CosmosClientManager() { }
 
 
-        static CosmosClientManager()
-        {
+        static CosmosClientManager() {
             //Type defaultTrace = Type.GetType("Microsoft.Azure.Cosmos.Core.Trace.DefaultTrace,Microsoft.Azure.Cosmos.Direct");
             //TraceSource traceSource = (TraceSource)defaultTrace.GetProperty("TraceSource").GetValue(null);
             //traceSource.Switch.Level = SourceLevels.Off;
@@ -47,8 +42,7 @@ namespace Microsoft.Solutions.CosmosDB.SQL
         private static readonly Lazy<CosmosClient> _instance =
             new Lazy<CosmosClient>(() => new CosmosClient(CosmosClientManager.DataconnectionString));
 
-        public static CosmosClient Instance
-        {
+        public static CosmosClient Instance {
             get { return _instance.Value; }
         }
     }
